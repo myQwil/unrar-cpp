bde8530f207080708b3ee1821253d4132a897692
Mike Will <myQwil@gmail.com>
2026-02-01 09:54:16 -0500

    isnt: add ConvertStringToBSTR manually

--- isnt.cpp
+++ isnt.cpp
@@ -30,6 +30,36 @@ DWORD WinNT()
 #include <wbemidl.h>
 #pragma comment(lib, "wbemuuid.lib")
 
+BSTR _com_util::ConvertStringToBSTR(const char* pSrc)
+{
+  if(!pSrc)
+    return NULL;
+
+  DWORD cwch;
+  BSTR wsOut(NULL);
+
+  if(cwch = ::MultiByteToWideChar(CP_ACP, 0, pSrc, -1, NULL, 0))
+  {
+    //get size minus NULL terminator
+    cwch--;
+    wsOut = ::SysAllocStringLen(NULL, cwch);
+
+    if(wsOut)
+    {
+      if(!::MultiByteToWideChar(CP_ACP, 0, pSrc, -1, wsOut, cwch))
+      {
+        if(ERROR_INSUFFICIENT_BUFFER == ::GetLastError())
+          return wsOut;
+        ::SysFreeString(wsOut);//must clean up
+
+        wsOut = NULL;
+      }
+    }
+  }
+
+  return wsOut;
+};
+
 static bool WMI_IsWindows10()
 {
   IWbemLocator *pLoc = NULL;
